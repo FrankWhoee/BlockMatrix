b17e7c26f938e6d82a1d214cc68dc1f91c983343
Andy Shen <andyshenproper@gmail.com>
2018-07-13 23:23:52 -0700

    Rudimentary GUI and ActionListeners

--- BlockMatrix/src/main/java/blockchain/app/blockmatrix/GUI.java
+++ BlockMatrix/src/main/java/blockchain/app/blockmatrix/GUI.java
@@ -1,13 +1,38 @@
 package blockchain.app.blockmatrix;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI {
     private JPanel panel1;
-    private JButton button1;
-    private JList list1;
-    private JButton button2;
-    private JList list2;
-    private JTextField textField1;
+    private JButton hitButton;
+    private JList inventory;
+    private JButton tradeButton;
+    private JList availableTrades;
+    private JTextField playerName;
+    private JButton takeWeaponButton;
+    private JTextField eventField;
     //You're mother gay and homosexual
+
+    public GUI() {
+        hitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        tradeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        takeWeaponButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                
+            }
+        });
+    }
 }
