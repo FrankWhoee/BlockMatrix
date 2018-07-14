42d4dafa5f972ac6f5ede9697acb8d3725ab53f8
Andy Shen <andyshenproper@gmail.com>
2018-07-14 11:34:49 -0700

    Imported all three button sprites

--- BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/gfx/Assets.java
+++ BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/gfx/Assets.java
@@ -7,10 +7,12 @@ public class Assets {
 	private static final int UICellSize = 64;
 	
 	public static BufferedImage[][] button;
+	public static BufferedImage eventBackground, statsBackground, enemyBackground;
 	
 	/*
 	 * [0][i] - inventory button
 	 * [1][i] - trade button
+	 * [2][i] - attack button
 	 * 
 	 * [i][0] - button rested
 	 * [i][1] - button hovered/active
@@ -18,14 +20,19 @@ public class Assets {
 	 */
 	
 	public static void init() {
-		SpriteSheet UISheet = new SpriteSheet(ImageLoader.loadImage("/uisheet.png"));
-		button = new BufferedImage[2][3];
+		SpriteSheet UISheet = new SpriteSheet(ImageLoader.loadImage("/mainscreenui.png"));
+		button = new BufferedImage[3][3];
 		
 		// UI sprites
 		
-		button[0][0] = UISheet.crop(UICellSize * 4, UICellSize * 4, UICellSize * 4, UICellSize * 2);
-		button[0][1] = UISheet.crop(UICellSize * 4, 0, UICellSize * 4, UICellSize * 2);
-		button[1][0] = UISheet.crop(UICellSize * 4, UICellSize * 6, UICellSize * 4, UICellSize * 2);
-		button[1][1] = UISheet.crop(UICellSize * 4, UICellSize * 2, UICellSize * 4, UICellSize * 2);
+		button[0][0] = UISheet.crop(UICellSize * 12, UICellSize * 2, UICellSize * 4, UICellSize * 2);
+		button[0][1] = UISheet.crop(UICellSize * 16, UICellSize * 2, UICellSize * 4, UICellSize * 2);
+		
+		button[1][0] = UISheet.crop(UICellSize * 12, UICellSize * 4, UICellSize * 4, UICellSize * 2);
+		button[1][1] = UISheet.crop(UICellSize * 16, UICellSize * 4, UICellSize * 4, UICellSize * 2);
+		
+		button[2][0] = UISheet.crop(0, UICellSize * 9, UICellSize * 4, UICellSize * 2);
+		button[2][1] = UISheet.crop(UICellSize * 4, UICellSize * 9, UICellSize * 4, UICellSize * 2);
+		button[2][2] = UISheet.crop(UICellSize * 8, UICellSize * 9, UICellSize * 4, UICellSize * 2);
 	}
 }
