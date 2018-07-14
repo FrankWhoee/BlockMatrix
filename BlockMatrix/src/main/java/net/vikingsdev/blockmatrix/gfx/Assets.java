f10757a80aa3b6fbfb07991b6332012cb6031050
Andy Shen <andyshenproper@gmail.com>
2018-07-14 13:10:29 -0700

    Imported Sprites

--- BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/gfx/Assets.java
+++ BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/gfx/Assets.java
@@ -7,7 +7,7 @@ public class Assets {
 	private static final int UICellSize = 64;
 	
 	public static BufferedImage[][] button;
-	public static BufferedImage eventBackground, statsBackground, enemyBackground;
+	public static BufferedImage eventBackground, statsBackground, enemyBackground, orc, forestBG;
 	
 	/*
 	 * [0][i] - inventory button
@@ -21,6 +21,7 @@ public class Assets {
 	
 	public static void init() {
 		SpriteSheet UISheet = new SpriteSheet(ImageLoader.loadImage("/mainscreenui.png"));
+		SpriteSheet MobSheet = new SpriteSheet(ImageLoader.loadImage("/mobandwp.png"));
 		button = new BufferedImage[3][3];
 		
 		// UI sprites
@@ -38,5 +39,8 @@ public class Assets {
 		eventBackground = UISheet.crop(UICellSize * 12, 0, UICellSize * 8, UICellSize * 2);
 		statsBackground = UISheet.crop(UICellSize * 13, UICellSize * 6, UICellSize * 7, UICellSize * 5);
 		enemyBackground = UISheet.crop(0, 0, UICellSize * 12, UICellSize * 9);
+		
+		forestBG = MobSheet.crop(0, 0, UICellSize * 12, UICellSize * 9);
+		orc = MobSheet.crop(UICellSize * 12, 0, UICellSize * 7, UICellSize * 9);
 	}
 }
