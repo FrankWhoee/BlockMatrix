9f973dbc578ab2f6dcab0e7b907421fcec9199a2
Andy Shen <andyshenproper@gmail.com>
2018-07-13 23:00:40 -0700

    Working on GUI

--- BlockMatrix/src/main/java/blockchain/app/blockmatrix/GUI.java
+++ BlockMatrix/src/main/java/blockchain/app/blockmatrix/GUI.java
@@ -1,5 +1,13 @@
 package blockchain.app.blockmatrix;
 
+import javax.swing.*;
+
 public class GUI {
-	//You're mother gay and homosexual
+    private JPanel panel1;
+    private JButton button1;
+    private JList list1;
+    private JButton button2;
+    private JList list2;
+    private JTextField textField1;
+    //You're mother gay and homosexual
 }
