a589031b4401bf485277b6b6d4f941c21aa50a41
Andy Shen <andyshenproper@gmail.com>
2018-07-14 02:00:31 -0700

    Tried to work on GUI but nothing successful

--- BlockMatrix/src/main/java/blockchain/app/blockmatrix/GUI.java
+++ BlockMatrix/src/main/java/blockchain/app/blockmatrix/GUI.java
@@ -1,19 +1,23 @@
 package blockchain.app.blockmatrix;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class GUI {
+	private JFrame frame;
     private JPanel panel1;
-    private JButton hitButton;
+    /*private JButton hitButton;
     private JList inventory;
     private JButton tradeButton;
     private JList availableTrades;
     private JTextField playerName;
     private JButton takeWeaponButton;
-    private JTextField eventField;
+    private JTextField eventField;*/
     
     // accesses the player in the app.java
     private Player player;
@@ -24,7 +28,14 @@ public class GUI {
         
         this.player = player;
         
-        playerName.setText(player.getName());
+        frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(new Dimension(1280, 720));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setContentPane(panel1);
+        
+        /*playerName.setText(player.getName());
         playerName.setEditable(false);
         
         hitButton.addActionListener(new ActionListener() {
@@ -44,6 +55,6 @@ public class GUI {
             public void actionPerformed(ActionEvent actionEvent) {
 
             }
-        });
+        });*/
     }
 }
