84efbdd6583997e88ed2633c8d9d5522e8b5dede
Andy Shen <andyshenproper@gmail.com>
2018-07-14 11:55:08 -0700

    Test code for rendering in Game

--- BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/Game.java
+++ BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/Game.java
@@ -70,7 +70,10 @@ public class Game implements Runnable{
 		
 		// render zone
 		
-		if(State.getState() != null) State.getState().render(g);
+		//if(State.getState() != null) State.getState().render(g);
+		g.drawImage(Assets.eventBackground,768,0,null);
+		g.drawImage(Assets.enemyBackground,0,32,null);
+		g.drawImage(Assets.statsBackground, 832, 384, null);
 		
 		// clean up
 		buffer.show();
