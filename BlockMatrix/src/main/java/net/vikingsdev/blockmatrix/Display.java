2cd4232055f4684ee7ec75ed053c7a067d0c4895
Andy Shen <andyshenproper@gmail.com>
2018-07-14 05:37:18 -0700

    Display Class

--- BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/Display.java
+++ BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/Display.java
@@ -1,5 +1,30 @@
 package net.vikingsdev.blockmatrix;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 public class Display {
 
+	private JFrame frame;
+	private String title;
+	private int width, height;
+	private Dimension size;
+	
+	public Display(int width, int height, String title) {
+		this.width = width;
+		this.height = height;
+		this.title = title;
+		this.size = new Dimension(width, height);
+		
+	}
+	
+	public void init() {
+		frame = new JFrame(title);
+		frame.setSize(size);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+	}
+	
 }
