e21d1c2d223f219d7ad97cb91a3e5597358cf0b4
Andy Shen <andyshenproper@gmail.com>
2018-07-14 09:38:37 -0700

    ConditionReader merge conflict fix

--- BlockMatrix/src/main/java/net/vikngsdev/blockmatrix/utils/ConditionReader.java
+++ BlockMatrix/src/main/java/net/vikngsdev/blockmatrix/utils/ConditionReader.java
@@ -1,8 +1,5 @@
-<<<<<<< Updated upstream:BlockMatrix/src/main/java/net/vikngsdev/blockmatrix/utils/ConditionReader.java
 package net.vikngsdev.blockmatrix.utils;
-=======
-package blockchain.app.blockmatrix;		//ok idk what this was meant to be for but if it was the condition thingy the interface thing is much easier i think
->>>>>>> Stashed changes:BlockMatrix/src/main/java/blockchain/app/blockmatrix/ConditionReader.java
+		//ok idk what this was meant to be for but if it was the condition thingy the interface thing is much easier i think
 
 public interface ConditionReader {
 	public void conditionMet();
