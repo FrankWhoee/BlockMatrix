6d84496ee54472e0b6c8d7acd08bef61bcd40fec
Andy Shen <andyshenproper@gmail.com>
2018-07-13 22:02:55 -0700

    Weapon Variable Init

--- BlockMatrix/src/main/java/blockchain/app/blockmatrix/Weapon.java
+++ BlockMatrix/src/main/java/blockchain/app/blockmatrix/Weapon.java
@@ -1,6 +1,13 @@
 package blockchain.app.blockmatrix;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Weapon extends Item{
 	//every weapon has
+
+    private String name;
+    private HashMap stats;
+    private ArrayList<Event> history;
 
 }
