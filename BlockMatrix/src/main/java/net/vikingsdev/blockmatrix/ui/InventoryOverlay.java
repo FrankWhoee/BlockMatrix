75c89be35db554345dbce8b6276c389c792df883
James Zhong <thebitspud@gmail.com>
2018-07-14 14:51:06 -0700

    Added code for various overlays (currently unused)

---
+++ BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/ui/InventoryOverlay.java
@@ -0,0 +1,27 @@
+package net.vikingsdev.blockmatrix.ui; 
+ 
+import java.awt.Graphics;
+
+import net.vikingsdev.blockmatrix.gfx.Assets; 
+ 
+public class InventoryOverlay extends UIOverlay { 
+	public InventoryOverlay(int x, int y, int width, int height) { 
+		super(x, y, width, height); 
+	} 
+	 
+	@Override 
+	public void init() {
+		uim.addObject(new UIButton(1024, 128, 256, 128, Assets.button[0], new UIListener() {
+			//Inventory
+			@Override
+			public void onClick() {
+				
+			}
+		}));  
+	}
+  
+	@Override 
+	public void render(Graphics g) {
+		uim.render(g);
+	} 
+} 
