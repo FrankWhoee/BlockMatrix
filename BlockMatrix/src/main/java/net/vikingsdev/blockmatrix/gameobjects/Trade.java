6addf0aae6e2051bef8ac754377871a00db5e565
Unknown <Minekitty17@gmail.com>
2018-07-15 12:06:19 -0700

    added some comments for trade

--- BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/gameobjects/Trade.java
+++ BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/gameobjects/Trade.java
@@ -13,6 +13,24 @@ public class Trade {
 		this.receiveIndex = receiveIndex;
 	}
 	
+	//boolean continue = ask use if they wanna continue;
+	//set up server-client
+	//while(continue) {		//there'll be an exit button all the time if they just want to bail on the trade except for after they confirm the trade
+		//select item, confirm
+		//writing(weaponToString(whatever weapon u chose));
+		//send ur file, recieve the other person's
+		//confirm = ask if you accept their weapon
+	
+		//if (confirm){
+			//player.getInventory.add(stringToWeapon);
+			//return
+		//}
+		//else
+			//continue
+	//}
+	//return;
+	
+	
 	public void completeTrade() {
 		Item sendItem = sender.getInventory().get(sendIndex);
 		Item receiveItem = receiver.getInventory().get(receiveIndex);
--- BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/utils/ConditionReader.java
+++ BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/utils/ConditionReader.java
@@ -1,6 +1,6 @@
 package net.vikingsdev.blockmatrix.utils;
 		//ok idk what this was meant to be for but if it was the condition thingy the interface thing is much easier i think
-
+		//wait wtf y is this back again we deleted this like 10 times alraedy
 public interface ConditionReader {
 	public void conditionMet();
 	
