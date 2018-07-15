9642f76c9c767e2b1be8db0e771c217038fea914
Andy Shen <andyshenproper@gmail.com>
2018-07-14 17:06:37 -0700

    Changed sprites for Trade

--- BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/ui/TradingOverlay.java
+++ BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/ui/TradingOverlay.java
@@ -13,7 +13,7 @@ public class TradingOverlay extends UIOverlay {
  
 	@Override 
 	public void init() {
-		uim.addObject(new UIButton(1024, 128, 256, 128, Assets.button[0], new UIListener() {
+		uim.addObject(new UIButton(644, 12, 64, 64, Assets.button[7], new UIListener() {
 			//Back
 			@Override
 			public void onClick() {
@@ -21,6 +21,23 @@ public class TradingOverlay extends UIOverlay {
 				game.getMouse().setUIM(game.getGameState().getUIM());
 			}
 		}));
+		
+		uim.addObject(new UIButton(720 - 128, 592 - 128, 256, 128, Assets.button[5], new UIListener() {
+			// accept button
+			@Override
+			public void onClick() {
+				game.getPlayer().setActiveSlot(-1);
+			}
+		}));  
+		
+		uim.addObject(new UIButton(720 - 128, 592, 256, 128, Assets.button[6], new UIListener() {
+			// trade button
+			@Override
+			public void onClick() {
+				setActive(false);
+				game.getMouse().setUIM(game.getGameState().getUIM());
+			}
+		}));  
 	} 
  
 	@Override 
