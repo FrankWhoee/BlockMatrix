01ae359b1a8ee67b0afee1f153b8569d9ac0d444
Unknown <huifrank@live.com>
2018-07-15 08:42:38 -0700

    Added toJson for Weapon class

--- BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/gameobjects/Weapon.java
+++ BlockMatrix/src/main/java/net/vikingsdev/blockmatrix/gameobjects/Weapon.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
 public class Weapon extends Item{
 	ArrayList<Event> history;
 	HashMap<String, Integer> stats;
@@ -14,7 +20,7 @@ public class Weapon extends Item{
 	String prefix;
 	String proper;
 	String suffix;
-	String name;
+	String name2;
 
 	AvailableEvents availableEvents = new AvailableEvents();
 	
@@ -26,7 +32,7 @@ public class Weapon extends Item{
     	this.availableClickTriggerable = availableEvents.getAvailableClickTriggerable();
     	this.availableKillTriggerable = availableEvents.getAvailableKillTriggerable();
     	kills = 0;
-        this.name = name;
+        this.name2 = name;
 		history = new ArrayList<Event>();
 		stats = new HashMap<String, Integer>();
         stats.put("Damage",1);		//i think weapon dmg needs to start at 1 lol, cuz for this mvp the point is all ur stats r tied up in ur weapon
@@ -108,11 +114,22 @@ public class Weapon extends Item{
     			break;
     	}
     }
-
+    
+    public String toJson() {
+    	Gson gson = new Gson();
+    	return gson.toJson(this);
+    }
+    
+    public static Weapon fromJson(String json) {
+    	Gson gson = new Gson();
+    	return gson.fromJson(json, Weapon.class);
+    }
+    
     /*List of stuff to add (roughly in high to low priority)
      * 		//done 		add int goblinsKilled or smt (another trackable)
      * 		//done		function to update the name whenever you get a new event (inside the addToHistory) (probs a for loop that goes through each completed event and adds each name into one of 3 parts (prefix, middle(proper), suffix), then adds the 3 parts together to create a full name (or you can just store the prefix, middle part (proper), suffix in separate strings and the getName method just combines them)(oh btw dont forget to add spaces and en dashes and stuff (i think easy way would be add an en dash between each prefix, proper, and suffix, and a space between the prefix proper and suffix (not technically correct but ez to implement)))
      * 		//done 		arraylist of all the events (grouped by events' interface) (will act as a checklist for this weapon to make sure we don't count one event (lol tbh calling them achivements make a lot more sense when u think about it but whatever) thingy twice)
      *		another potential stat to add would be maximum click speed, but these extra features r low priority
+     *					peanus cake
      */
 }
